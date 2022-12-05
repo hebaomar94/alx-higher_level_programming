fbed3824dbf6375ad6fb36357dc146e31dac3f0e
hebaomar94 <hebaomar93.ho@gmail.com>
2022-12-05 15:29:22 -0800

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0 :
-        print("none")
-    if idx > (my_lis):
-        print("none")
-    else
-    print(my_list[idx])
 
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list) - 1:
+        return 'None'
+    else:
+        return my_list[idx]
