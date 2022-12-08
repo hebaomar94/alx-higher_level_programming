7a690321418134dad2d0c958028b34fec94cba74
hebaomar94 <hebaomar93.ho@gmail.com>
2022-12-08 11:41:23 -0800

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -9,7 +9,7 @@ def roman_to_int(roman_string):
           if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
               roman_n += roman_d[roman_string[j]] - 2 * \
                       roman_d[roman_string[j - 1]]
-         else:
-             roman_n += roman_d[roman_string[j]]
+          else:
+              roman_n += roman_d[roman_string[j]]
 
-return roman_n
+     return roman_n
