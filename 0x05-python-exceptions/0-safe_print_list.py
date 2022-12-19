ae504013167497d2c9fc549cf739225a9c17005d
hebaomar94 <hebaomar93.ho@gmail.com>
2022-12-19 10:13:52 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0 
-        for x in my_list:
-            print (x)
-            count += 1
-        print (count)
-    except Exception as e :
-        print(e, 'Error!') 
+        try:
+            count = 0
+            for i in range(x):
+                count += 1
+                print(my_list[i], end=' ')
+                                                        
+                print()
+                print (count)
+                return
+        except Exception as e :
+        print(e)
 
 
