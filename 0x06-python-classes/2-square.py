184fd50f7bc19515d663a9f0bf17854839f7eba8
hebaomar94 <hebaomar93.ho@gmail.com>
2022-12-22 23:49:13 +0000

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
-"""A module that contain Square class"""
 
-	class Square:
-		 """Represents a 2D polygon with 4 sizes and perpendicular sides
+
+
+"""A module that contain Square class
+
+"""
+
+
+
+
+
+class Square:
+
+    """Represents a 2D polygon with 4 sizes and perpendicular sides
 
     
 
@@ -25,10 +35,17 @@
          ValueError: if ``__size`` attribute is less than zero.
 
     """
-		def __init__(self, size=0):
-			if type(size) != int:
-				raise TypeError('size must be an integer')
-			elif size < 0:
-				raise ValueError('size must be >= 0')
-			else:
-				self.__size = size
+
+    def __init__(self, size=0):
+
+        if type(size) != int:
+
+            raise TypeError('size must be an integer')
+
+        elif size < 0:
+
+            raise ValueError('size must be >= 0')
+
+        else:
+
+            self.__size = size
