057d22c19b79a25580d48c56f84f18cc6cb44f8f
hebaomar94 <hebaomar93.ho@gmail.com>
2022-12-22 23:32:24 +0000

    1- git commit -m 1-square.py git add . clear git commit -m 1-

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""this modul contains Square class"""
+
+
+
+
+
+class Square:
+
+    """a class Square that defines a square with private instance attribute
+
+    
+
+    Args:
+
+         value (int): a value that initializes ``__size`` attribute
+
+    
+
+    Attributes:
+
+         __size (int): a private instance attribute
+
+                       The size of a square is crucial for a square,
+
+                       many things depend of it (area computation, etc.),
+
+                       so i, as class builder, must control the type and
+
+                       value of this attribute.
+
+                       One way to have the control is to keep it privately.
+
+    """
+
+	def __init__(self, value):
+	   self.__size = value
