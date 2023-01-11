92bb6c356c76972a1c663d15b183f4dd7cce7dc3
hebaomar94 <hebaomar93.ho@gmail.com>
2023-01-11 15:52:04 -0800

    8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle that inherits from BaseGeometry."""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+a module that imports a base class `BaseGeometry` from 7-base_geometry module
+and does some stuff in Rectangle subclass.
+"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Represent a rectangle using BaseGeometry."""
+    """
+    Represents a geometry object.
+    """
 
-      def __init__(self, width, height):
-         """Intialize a new Rectangle.
-
-         Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
+    def __init__(self, width, height):
         """
-         self.integer_validator("width", width)
-         self.__width = width
-         self.integer_validator("height", height)
-         self.__height = height
+        Initializes a new Rectangle geometry
+        object with the given width and height.
+
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        """
+        # super().__init__()
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
