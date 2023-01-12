55f8492239cd12735670ea341b0383f8bc99fdcb
hebaomar94 <hebaomar93.ho@gmail.com>
2023-01-11 16:29:51 -0800

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,14 +1,40 @@
 #!/usr/bin/python3
-"""Defines a class MyInt that inherits from int."""
+'''A module containing a rebellious int.
+'''
 
 
 class MyInt(int):
-        """Invert int operators == and !=."""
+    '''Represents a rebellious integer object.
+    '''
 
-            def __eq__(self, value):
-                """Override == opeartor with != behavior."""
-                return self.real != value
+    def __eq__(self, other):
+        '''Checks if the given value is not equal to the
+        value of this object.
 
-            def __ne__(self, value):
-                """Override != operator with == behavior."""
-                return self.real == value
+
+        Args:
+            other (MyInt): The value to be compared against.
+
+        Returns:
+            bool: True if the value is not equal to the value
+            stored by this object.
+
+        '''
+
+        return super().__ne__(other)
+
+
+    def __ne__(self, other):
+
+        '''Checks if the given value is equal to the value
+        of this object.
+
+        Args:
+            other (MyInt): The value to be compared against.
+
+        Returns:
+            bool: True if the value is equal to the value
+            stored by this object.
+
+        '''
+        return super().__eq__(other)
